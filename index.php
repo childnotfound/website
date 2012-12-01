c7c7fdf832f2d494ea7ca9be84f97ac4a862e229
hSATAC <hsatac@gmail.com>
2012-12-01 16:58:14 +0800

    extract config and set db utf-8

--- index.php
+++ index.php
@@ -5,11 +5,18 @@ require __DIR__ . '/vendor/kevinsperrine/idiorm/src/Orm.php';
 require __DIR__ . '/models/missing_children.class.php';
 require 'config.php';
 
+ORM::configure('mysql:host=localhost;dbname='.$config['db']['database']);
+ORM::configure('username', $config['db']['username']);
+ORM::configure('password', $config['db']['password']); 
+ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
 $app = new \Slim\Slim();
 
 # index
 $app->get('/', function() use($app) {
   echo "Hello World";
+  $child = Model::factory('MissingChildren')->orderByExpr('RAND()')->findOne();
+  var_dump($child);
 });
 
 $app->get('/404', function() use($app) {
